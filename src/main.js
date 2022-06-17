fdd596659f1199028668232f669640657923fe31
darkhao <850199417@qq.com>
2022-06-17 21:01:41 +0800

    完成了css和less功能

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { marquee } from "./marquee.js";
 import { tab } from "./tab.js";
+import './styles/index.css'
+import './styles/index.less'
+
 marquee()
 tab()
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,33 @@ const { join } = require("path")
     // 引入自动生成 html 的插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
+    mode: 'development',
     entry: "./src/main.js",
     output: {
         path: join(__dirname, "lib"), // 出口路径
-        filename: "index.js" // 出口文件名
+        filename: "index.js", // 出口文件名
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: 'index.html'
         })
-    ]
+    ],
+    devServer: {
+        open: true,
+        port: 30000
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        }, {
+            test: /\.less$/,
+            use: ["style-loader", "css-loader", 'less-loader']
+        }, {
+            test: /\.(png|jpg|gif|jpeg)$/i,
+            type: 'asset'
+        }]
+    }
 }
