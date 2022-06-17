fef31ff099c2c6bc4028da2c6e11c0775395c42c
darkhao <850199417@qq.com>
2022-06-17 20:35:19 +0800

    完成了生成index

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const { join } = require("path")
+    // 引入自动生成 html 的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: "./src/main.js",
     output: {
         path: join(__dirname, "lib"), // 出口路径
         filename: "index.js" // 出口文件名
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        })
+    ]
 }
